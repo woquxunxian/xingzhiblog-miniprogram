57aaab47b1cbce120e96d36731c39d710311ce84
wsh <1252136236@qq.com>
2020-12-06 11:57:50 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,16 @@
+//app.js
+App({
+  onLaunch: function () {
+    wx.getSystemInfo({
+      success: e => {
+        this.globalData.StatusBar = e.statusBarHeight;
+        let custom = wx.getMenuButtonBoundingClientRect();
+        this.globalData.Custom = custom;  
+        this.globalData.CustomBar = custom.bottom + custom.top - e.statusBarHeight;
+      }
+    })
+  },
+  globalData: {
+    userInfo: null
+  }
+})
